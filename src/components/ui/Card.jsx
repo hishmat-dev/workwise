a7326938216e83dee4393e3b89b0145a6b9a0294
hishmat-dev <developerhishmat1@gmail.com>
2025-05-08 14:20:12 +0500

    set all

--- src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card({ children, className = "", ...props }) {
-  return (
-    <div className={`rounded-xl border bg-card text-card-foreground shadow ${className}`} {...props}>
-      {children}
-    </div>
-  )
-}
-
-export default Card
